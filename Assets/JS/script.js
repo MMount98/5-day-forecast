2c17dca74f6f2714cd475ed5440341f673ed8f24
Michael Mount <mount.photos@gmail.com>
2023-02-22 10:28:06 -0700

    Added forecast data

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -2,7 +2,7 @@ var currentDay = $("span");
 var currentStats = $(".currentDay");
 var day = dayjs();
 var requestUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=denver&appid=2061c6c3acd7e8fc81514bd609fb308e&units=imperial";
+  "https://api.openweathermap.org/data/2.5/forecast?q=denver&appid=2061c6c3acd7e8fc81514bd609fb308e&units=imperial";
 
 function getApi() {
   fetch(requestUrl)
@@ -11,9 +11,14 @@ function getApi() {
     })
     .then(function (data) {
       console.log(data);
-      var currentName = data.name;
-      var currentArray = [data.main.temp, data.wind.speed, data.main.humidity];
-
+      var currentName = data.city.name;
+      console.log(currentName);
+      var currentArray = [
+        data.list[0].weather[0].icon,
+        Math.round(data.list[0].main.temp),
+        Math.round(data.list[0].wind.speed),
+        data.list[0].main.humidity,
+      ];
       currentDay.text(currentName + " " + day.format("dddd, MMMM D"));
 
       for (var i = 0; i < currentArray.length; i++) {
@@ -21,6 +26,7 @@ function getApi() {
 
         currentStats.append(createLi);
       }
+      console.log(currentArray);
     });
 }
 
