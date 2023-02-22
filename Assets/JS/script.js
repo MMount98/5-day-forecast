254ee481c4837d95618c706ad9491b2473e7a36e
Michael Mount <mount.photos@gmail.com>
2023-02-21 23:33:31 -0700

    Added current stats

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -1,3 +1,6 @@
+var currentDay = $("span");
+var currentStats = $(".currentDay");
+var day = dayjs();
 var requestUrl =
   "https://api.openweathermap.org/data/2.5/weather?q=denver&appid=2061c6c3acd7e8fc81514bd609fb308e&units=imperial";
 
@@ -8,6 +11,16 @@ function getApi() {
     })
     .then(function (data) {
       console.log(data);
+      var currentName = data.name;
+      var currentArray = [data.main.temp, data.wind.speed, data.main.humidity];
+
+      currentDay.text(currentName + " " + day.format("dddd, MMMM D"));
+
+      for (var i = 0; i < currentArray.length; i++) {
+        var createLi = $("<li></li>").text(currentArray[i]);
+
+        currentStats.append(createLi);
+      }
     });
 }
 
