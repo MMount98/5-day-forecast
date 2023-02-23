867659f7062dbfd85804a1c1c77b45e48bd94e5a
Michael Mount <mount.photos@gmail.com>
2023-02-22 22:37:19 -0700

    added day for box

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -36,8 +36,8 @@ function getApi() {
       var dayOneTemp = Math.round(data.list[8].main.temp);
       var dayOneWind = Math.round(data.list[8].wind.speed);
       var dayOneHum = data.list[8].main.humidity;
-      console.log(dayOneHum);
 
+      //Day One box
       dayOneBox.text(dayOneDate);
 
       var dayOneImg = $("<img></img").attr(
@@ -51,12 +51,13 @@ function getApi() {
       var createOneTemp = $("<p></p>").text("Temp: " + dayOneTemp + "\xB0");
       dayOneBox.append(createOneTemp);
 
-      var createOneWind = $("<p></p>").text("Temp: " + dayOneWind + "MPH");
+      var createOneWind = $("<p></p>").text("Wind: " + dayOneWind + "MPH");
       dayOneBox.append(createOneWind);
 
       var createOneHum = $("<p></p>").text("Humidity: " + dayOneHum + "%");
       dayOneBox.append(createOneHum);
 
+      //day Two Box
       var dayTwoBox = $(".dayTwoBox h2");
       var dayTwoDate = day.add(2, "d").format("M/D/YY");
       var dayTwoTemp = Math.round(data.list[16].main.temp);
@@ -76,11 +77,63 @@ function getApi() {
       var createTwoTemp = $("<p></p>").text("Temp: " + dayTwoTemp + "\xB0");
       dayTwoBox.append(createTwoTemp);
 
-      var createTwoWind = $("<p></p>").text("Temp: " + dayTwoWind + "MPH");
+      var createTwoWind = $("<p></p>").text("Wind: " + dayTwoWind + "MPH");
       dayTwoBox.append(createTwoWind);
 
       var createTwoHum = $("<p></p>").text("Humidity: " + dayTwoHum + "%");
       dayTwoBox.append(createTwoHum);
+
+      //Day Three Box
+      var dayThreeBox = $(".dayThreeBox h2");
+      var dayThreeDate = day.add(3, "d").format("M/D/YY");
+      var dayThreeTemp = Math.round(data.list[24].main.temp);
+      var dayThreeWind = Math.round(data.list[24].wind.speed);
+      var dayThreeHum = data.list[24].main.humidity;
+
+      dayThreeBox.text(dayThreeDate);
+
+      var dayThreeImg = $("<img></img").attr(
+        "src",
+        "http://openweathermap.org/img/wn/" +
+          data.list[24].weather[0].icon +
+          "@2x.png"
+      );
+      dayThreeBox.append(dayThreeImg);
+
+      var createThreeTemp = $("<p></p>").text("Temp: " + dayThreeTemp + "\xB0");
+      dayThreeBox.append(createThreeTemp);
+
+      var createThreeWind = $("<p></p>").text("Wind: " + dayThreeWind + "MPH");
+      dayThreeBox.append(createThreeWind);
+
+      var createThreeHum = $("<p></p>").text("Humidity: " + dayThreeHum + "%");
+      dayThreeBox.append(createThreeHum);
+
+      //Day Four Box
+      var dayFourBox = $(".dayFourBox h2");
+      var dayFourDate = day.add(4, "d").format("M/D/YY");
+      var dayFourTemp = Math.round(data.list[32].main.temp);
+      var dayFourWind = Math.round(data.list[32].wind.speed);
+      var dayFourHum = data.list[32].main.humidity;
+
+      dayFourBox.text(dayFourDate);
+
+      var dayFourImg = $("<img></img").attr(
+        "src",
+        "http://openweathermap.org/img/wn/" +
+          data.list[32].weather[0].icon +
+          "@2x.png"
+      );
+      dayFourBox.append(dayFourImg);
+
+      var createFourTemp = $("<p></p>").text("Temp: " + dayFourTemp + "\xB0");
+      dayFourBox.append(createFourTemp);
+
+      var createFourWind = $("<p></p>").text("Wind: " + dayFourWind + "MPH");
+      dayFourBox.append(createFourWind);
+
+      var createFourHum = $("<p></p>").text("Humidity: " + dayFourHum + "%");
+      dayFourBox.append(createFourHum);
     });
 }
 
